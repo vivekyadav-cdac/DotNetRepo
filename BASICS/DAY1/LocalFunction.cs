fb1b27c37a0167006f8c49c1086c64be3f157ec2
vivekyadav-cdac <vivekyadavcdac@gmail.com>
2025-06-25 18:11:31 +0530

    initial commit to this repo

---
+++ BASICS/DAY1/LocalFunction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY1
+{
+    
+    internal class LocalFunction
+    {
+        
+        public void OuterFunction()
+        {
+            int i = 10;
+
+            Console.WriteLine("accessing outer variable in outer function only value is: "+i);
+
+        void InnerFunction()
+        {
+                i++;
+            Console.WriteLine("accessing outer variable in inner function  after changing value is :" + i);
+        }
+
+
+            Console.WriteLine("calling inner function");
+            InnerFunction();
+        
+        }
+
+        /*===================================================================================================================*/
+        /* We can access outer variable inside the local function but we can't access the variable of local function in outer scope
+         * The access specifier of the local variable by default (implicitly) : private
+         * So they cannot be accessed outside the class 
+         * implicitly mean we cannot give any explicit access specifiers
+         * it can only be called from outer function
+         */
+        /*===================================================================================================================*/
+
+        // Trying to access local function from outside the Outer function
+
+            // InnerFunction(); --->> this is giving error because local function can only be called from inside the containing outer function
+        
+
+        public void SecondOuterFunction()
+        {
+            string str = "second outer function string";
+
+            /* also here static string cannot be defined as in c# static variables are defined  at class level only */
+
+            // static string StaticString = "this is outer function static string";
+
+            //code:
+
+            //static void StaticInnerFunction()
+            //{
+            //    // Trying to access localvariable of outerfunction ---> error : because static function can only access static variables
+            //    Console.WriteLine("accessing string defined in secondouterfunction by its static local function, string :"+ str);
+            //}
+
+            /*=====================================================================================================================*/
+            /*We can access the non static variable defined in outer function by passing it as a parameter to the local function
+             * otherwise the static local function can only access the static data members defined at  class level
+             */
+            /*=====================================================================================================================*/
+
+            static void StaticInnerFunction(string str)
+            {
+                // Trying to access localvariable of outerfunction ---> is accessible because essentially it is creating a new localvariable inside the local function
+                // StaticInnerFunction(string str) ---> string str = str;
+                Console.WriteLine("accessing string defined in secondouterfunction by its static local function, string :" + str);
+            }
+            StaticInnerFunction(str);
+        }
+    }
+}
---
+++ BASICS/DAY1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace DAY1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Class1 obj = new Class1();
+            obj.Display();
+
+            obj.Display("overloaded");
+        }
+
+        static void Main2(string[] args)
+        {
+
+            Class1 class1 = new Class1();
+            
+            // here we are passing only two values
+            // so for third param default value c=0 is being used
+            Console.WriteLine(class1.Add(1,2));
+
+            // here we are passing three values 
+            // so all of them are used 
+            Console.WriteLine(class1.Add(3,4,5));
+
+            /*==========================================================================================*/
+            // functions with default values give same functionality
+            // as overloaded functions as long as datatype of params and return type remains same
+            /*==========================================================================================*/
+        }
+
+        static void Main(string[] args)
+        {
+            LocalFunction localFunction = new LocalFunction();
+
+            localFunction.OuterFunction();
+
+            //Trying to access localFunction from LocalFunction class
+                //localFunction.InnerFunction() -->> gives error as a local function is implicitly private and not available outside the class
+
+            localFunction.SecondOuterFunction();
+        }
+    }
+
+    public class Class1
+    {
+        //public void Display()
+        //{
+        //    Console.WriteLine("display called");
+        //}
+
+        //public void Display(string s)
+        //{
+        //    Console.WriteLine("display called with :" + s);
+        //}
+
+        /*===========================================================*/
+
+        /* whenever we need to overload 
+         we should prefer using function with default values instead
+         In function default values should be given from right to left
+           */
+
+        /*===========================================================*/
+
+        // Funtion with default params
+        public void Display(string s = "")
+        {
+            Console.WriteLine("display called "+s);
+        }
+
+        // Function for add with default params
+
+        public int Add(int a = 0, int b = 0, int c = 0)
+        {
+            return a + b + c;
+        }
+    }
+}
